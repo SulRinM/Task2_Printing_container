085ac85ba77be73651e6e3c5c085dbbde49d2d4c
SulRinM <risultan@mail.ru>
2023-03-06 00:11:35 +0300

    Добавьте файлы проекта.

---
+++ Task2_Printing_container/Task2_Printing_container.cpp
@@ -0,0 +1,54 @@
+﻿/*
+Задача 2. Печать контейнера
+Описание
+Нужно реализовать шаблонную функцию, которая печатает содержимое контейнера.
+
+Контейнер может быть любым:
+
+std::set
+std::vector
+std::list
+Пример правильной работы программы
+std::set<std::string> test_set = { "one", "two", "three", "four" };
+print_container(...); // four one three two. помните почему такой порядок? :)
+
+std::list<std::string> test_list = { "one", "two", "three", "four" };
+print_container(...); // one, two, three, four
+
+std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+print_container(...); // one, two, three, four
+
+*/
+#include <iostream>
+#include <string>
+#include <list>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+
+template<typename T>
+void print_container(T a)
+{
+    for (const auto& i : a)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+    
+    return 0;
+}
+
